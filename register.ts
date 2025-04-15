98d9eb8beb07ccdb1bf701ad04d213cd1f672fab
Neel Radhakrishnan <neel@rdkr.uk>
2025-04-15 22:12:19 +0100

    biome

--- register.ts
+++ register.ts
@@ -1,21 +1,22 @@
-
-import { REST, Routes } from 'discord.js';
+import { REST, Routes } from "discord.js";
 
 const commands = [
-  {
-    name: 'avo',
-    description: 'activate avo bot!',
-  },
+	{
+		name: "avo",
+		description: "activate avo bot!",
+	},
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN!);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
 
 try {
-  console.log('Started refreshing application (/) commands.');
+	console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands });
+	await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+		body: commands,
+	});
 
-  console.log('Successfully reloaded application (/) commands.');
+	console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
-  console.error(error);
+	console.error(error);
 }
